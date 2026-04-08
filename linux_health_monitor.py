c3a2dded11b94e2f075f3dd9b0f13149e98311bb
chrisd1113 <chrisdouglas1113@gmail.com>
2026-04-08 14:03:23 -0400

    Fixed secondary call to get_system_metrics, fixed minior spelling error, and added keyword for cpu_percent function

--- linux_health_monitor.py
+++ linux_health_monitor.py
@@ -6,7 +6,7 @@ import time
 def get_system_metrics():
 	metrics = {
 		"timestamp": datetime.datetime.now().isoformat(),
-		"cpu_percent": psutil.cpu_percent(1),
+		"cpu_percent": psutil.cpu_percent(interval=1),
 		"memory_percent": psutil.virtual_memory().percent,
 		"disk_percent": psutil.disk_usage("/").percent,
 		"running_processes": len(psutil.pids()),
@@ -17,9 +17,8 @@ def get_system_metrics():
 
 if __name__ == "__main__":
 
-	print("=== Linux Health Monior Starting ===")
+	print("=== Linux Health Monitor Starting ===")
 	for i in range(3):
-		get_system_metrics()
 		print(get_system_metrics())
 		time.sleep(2)
 	print("=== Monitoring Complete ===")
